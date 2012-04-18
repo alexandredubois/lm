bf27b5a28080d4fd8c4aaaf6986197e843d48cb2
Alexandre Dubois <contact@alexandredubois.com>
2012-04-18 14:27:18 +0200

    Hash function modify to handle more CPU architecture

--- lm.py
+++ lm.py
@@ -112,34 +112,31 @@ def filelist( dir, recurs=True, *ext):
 def hashFile(name):
     try:
 
-        longlongformat = 'q'  # long long
-        bytesize = struct.calcsize(longlongformat)
+        longlongformat = 'Q'  # unsigned long long little endian
+        bytesize = struct.calcsize(longlongformat) 
+        format= "<%d%s" % (65536//bytesize, longlongformat)
+        
+        f = open(name, "rb") 
+        
+        filesize = os.fstat(f.fileno()).st_size
+        hash = filesize 
 
-        f = open(name, "rb")
+        if filesize < 65536 * 2: 
+           print "SizeError" 
 
-        filesize = os.path.getsize(name)
-        hash = filesize
+        buffer= f.read(65536)
+        longlongs= struct.unpack(format, buffer)
+        hash+= sum(longlongs)
+        
+        f.seek(-65536, os.SEEK_END) # size is always > 131072
+        buffer= f.read(65536)
+        longlongs= struct.unpack(format, buffer)
+        hash+= sum(longlongs)
+        hash&= 0xFFFFFFFFFFFFFFFF
 
-        if filesize < 65536 * 2:
-                return "SizeError"
-
-        for x in range(65536/bytesize):
-                buffer = f.read(bytesize)
-                (l_value,)= struct.unpack(longlongformat, buffer)
-                hash += l_value
-                hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number
-
-        f.seek(max(0,filesize-65536),0)
-        for x in range(65536/bytesize):
-                buffer = f.read(bytesize)
-                (l_value,)= struct.unpack(longlongformat, buffer)
-                hash += l_value
-                hash = hash & 0xFFFFFFFFFFFFFFFF
-
-        f.close()
-        returnedhash =  "%016x" % hash
+        f.close() 
+        returnedhash =  "%016x" % hash 
         return returnedhash
-
     except(IOError):
             return "IOError"
 
