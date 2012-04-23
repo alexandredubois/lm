2ba3608e77feb781a03416d5da55712be0190c04
Alexandre Dubois <contact@alexandredubois.com>
2012-04-23 15:41:57 +0200

    The number of rows displayed in the HTML page is now set to 50 by default

--- bootstrap/js/DT_bootstrap.js
+++ bootstrap/js/DT_bootstrap.js
@@ -99,6 +99,7 @@ $(document).ready(function() {
 	$('#moviesTable').dataTable( {
 		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		"sPaginationType": "bootstrap",
+		"iDisplayLength": 50,
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
 		}
